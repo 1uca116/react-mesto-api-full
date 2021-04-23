5963a50762461bee295d00f5affc08e6ff023c47
1uca116 <l.bakirova116@gmail.com>
2021-04-24 01:26:06 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards');
 const NotFoundError = require('./errors/not-found-error');
 require('dotenv').config();
+const cors = require('cors');
 
 const { celebrate, Joi } = require('celebrate');
 
@@ -16,6 +17,9 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
