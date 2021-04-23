0e2b332494153259686a0d9f6516d16ea8ad1c6c
1uca116 <l.bakirova116@gmail.com>
2021-04-24 01:54:09 +0300

    logging

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, secret);
   } catch (err) {
-    throw new UnauthorizedError(`Необходима авторизация, ${secret}`);
+    throw new UnauthorizedError(`Необходима авторизация`);
   }
 
   req.user = payload;
